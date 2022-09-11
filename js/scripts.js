80e0990c87222fc876a6de1efe0bf3f42cc88b9f
Noah Cowan <nsncowan@gmail.com>
2022-09-11 16:31:07 -0700

    WIP add html and js to project.
    Co-authored by: Remy Flores
    <remyfranciscoflores@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -24,9 +24,29 @@ TaskList.prototype.deleteTask = function(id) {
 
 
 // Task Logic
-function Task (taskName, taskPlace, priority, completed) {
+function TaskOn (taskName, taskPlace, priority, completed) {
   this.taskName = taskName;
   this.taskPlace = taskPlace;
   this.priority = priority;
   this.completed = completed;
 }
+
+// UI //
+let taskList = new TaskList();
+
+function formSubmission(event) {
+  event.preventDefault();
+  const inputName = document.querySelector("input#newTaskName").value;
+  const inputPlace = document.querySelector("input#newTaskPlace").value;
+  const priority = document.querySelector("input#newTaskPriority").value;
+  const done = document.querySelector("input#newTaskCompleted").value;
+  let newArr = new TaskOn(inputName, inputPlace, priority, done);
+  taskList.addTask(newArr);
+  TaskOn(taskList);
+  console.log(taskList.tasks);
+
+};
+
+window.addEventListener("load", function() {
+  document.querySelector("form#newTask").addEventListener("submit", formSubmission);
+})
